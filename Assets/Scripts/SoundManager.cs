1f3cba0214631a8492d8c96846081f175aba4b1d
Adam Moore <amoore@uat.edu>
2020-03-30 12:20:56 -0700

    Lecture 12.1Completed

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip screamClip;
+    public AudioClip doorClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            audioSource.clip = screamClip;
+            audioSource.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            audioSource.clip = doorClip;
+            audioSource.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            audioSource.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Pause();
+            }
+            else
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
